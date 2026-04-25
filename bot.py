0b1af3c35bf591219816471e3b7582061034cba9
yagubiskenderov483-code <yagubiskenderov483@gmail.com>
2026-04-25 11:12:34 +0500

    Fix quote styles in bot.py configuration
    Updated string quotes from smart quotes to standard quotes.

--- bot.py
+++ bot.py
@@ -7,13 +7,13 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(**name**)
 
 BOT_TOKEN = "8767675859:AAEYrMnVMAZjI7rBEc1QLNpHs7rVlUIv-0Q"
-ADMIN_ID     = 8726084830
-BOT_USERNAME = “GiftDeals_Robot”
-MANAGER_URL  = “https://t.me/GiftDealsManager”
-MANAGER_TAG  = @GiftDealsManager
-CRYPTO_ADDR  = “UQDGN5pfjPxorFyjN2xha84bapuADDtPcRofNDJ4dK2YXxZd”
-CRYPTO_BOT   = “https://t.me/send?start=IVbfPL7Tk4XA”
-CARD_NUM     = “+79041751408”
+ADMIN_ID = 8726084830
+BOT_USERNAME = "GiftDeals_Robot"
+MANAGER_URL = "https://t.me/GiftDealsManager"
+MANAGER_TAG = "@GiftDealsManager"
+CRYPTO_ADDR = "UQDGN5pfjPxorFyjN2xha84bapuADDtPcRofNDJ4dK2YXxZd"
+CRYPTO_BOT = "https://t.me/send?start=IVbfPL7Tk4XA"
+CARD_NUM = "+79041751408"
 CARD_NAME    = “Александр Ф.”
 CARD_BANK_RU = “ВТБ”
 CARD_BANK_EN = “VTB”
