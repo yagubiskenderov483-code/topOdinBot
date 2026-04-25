8a23a97f0e303c8f2704dc725cfd15cecd691ee7
yagubiskenderov483-code <yagubiskenderov483@gmail.com>
2026-04-25 11:50:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -549,8 +549,8 @@ def build_deal_text(deal_id, d, creator_tag, partner_tag, lang, joined=False, is
                     ]
                 else:
                      bank = card_bank(lang)
-lines + = [
-    f"<b>{Ecrd} {'СБП / Карта' if ru else 'Card / Phone'} {bank}:</b>",
+lines
+    f" {Ecrd} {'СБП / Карта' if ru else 'Card / Phone'} {bank}:</b>",
     f"<blockquote>{R(ru,'Номер','Number')}: <code>{CARD_NUM}</code>\n{R(ru,'Получатель','Recipient')}: {CARD_NAME}\n{R(ru,'Банк','Bank')}: {bank}</blockquote>",
 ]
 text_ru = 'После перевода нажмите "Я оплатил"'
