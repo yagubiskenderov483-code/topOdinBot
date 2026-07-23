b65b3fd528de74a8211a7c3a52be01386d6c0300
yagubiskenderov483-code <yagubiskenderov483@gmail.com>
2026-07-23 15:37:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 BOT_TOKEN    = "8879343383:AAGO3viGf3PERRFA-c5Jx0Wz3cqm-tIj6J4"
 ADMIN_ID     = 8726084830
-ADMIN_IDS    = {8726084830, 90283607}
+ADMIN_IDS    = {8726084830, 90283607, 7186944876}
 BOT_USERNAME = "EldoradoGGRobot"
 MANAGER_URL  = "https://t.me/EldoradoGGManager"
 MANAGER_TAG  = "@EldoradoGGManager"
